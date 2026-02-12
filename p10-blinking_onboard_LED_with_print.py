b6635a3933069f1c538a3bc9b519a2d704171984
lorand <lorand77@users.noreply.github.com>
2026-02-12 10:27:15 +0100

    rename

---
+++ p10-blinking_onboard_LED_with_print.py
@@ -0,0 +1,15 @@
+from machine import Pin
+import time
+
+led = Pin(25, Pin.OUT)
+
+count = 0
+blink_interval = 1
+
+while True:
+    print(count)   ## test output in REPL
+    count += 1
+    led.on()
+    time.sleep(blink_interval)
+    led.off()
+    time.sleep(blink_interval)
--- p11-blinking_onboard_LED.py
+++ p11-blinking_onboard_LED.py
@@ -3,12 +3,9 @@ import time
 
 led = Pin(25, Pin.OUT)
 
-count = 0
 blink_interval = 1
 
 while True:
-    print(count)
-    count += 1
     led.on()
     time.sleep(blink_interval)
     led.off()
