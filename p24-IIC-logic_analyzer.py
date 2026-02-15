fc3f5ac9c32dbfa750522ba6a9dfee12b9ed3efd
lorand <lorand77@users.noreply.github.com>
2026-02-15 12:16:50 +0100

    add more logic analyzer pics

--- p24-IIC-logic_analyzer.py
+++ p24-IIC-logic_analyzer.py
@@ -19,3 +19,18 @@ x = i2c.readfrom(DS3231_ADDR, 1)
 # marker 2:   ack (0)
 # marker 3:   data 8 bits (0b01010000)
 # marker 4:   nack (1)
+
+x = i2c.readfrom_mem(DS3231_ADDR, 0x00, 1)
+[bin(y) for y in list(x)]
+#['0b1011001']
+# marker 0:   device address 1101000 (7 bits)
+# marker 1:   write bit 0
+# marker 2:   ack (0)
+# marker 3:   register address 0x00 (8 bits)
+# marker 4:   ack (0)
+# stop + start 
+# marker 5:   device address 1101000 (7 bits)
+# marker 6:   read bit 1
+# marker 7:   ack (0)
+# marker 8:   data 8 bits (0b01011001)
+# marker 9:   nack (1)
